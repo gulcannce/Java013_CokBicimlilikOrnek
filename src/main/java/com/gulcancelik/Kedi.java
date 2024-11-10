2f74d50413cb5a06fa314ed09de7719c4472e36d
gulcannce <gulcanncee@gmail.com>
2024-11-10 22:55:14 +0300

    Initial commit

---
+++ src/main/java/com/gulcancelik/Kedi.java
@@ -0,0 +1,13 @@
+package com.gulcancelik;
+
+public class Kedi extends Hayvan{
+
+    public Kedi() {
+        System.out.println("Kedi");
+    }
+
+    @Override
+    public void sesVer(){
+        System.out.println("Kedi : Miyav miyav.");
+    }
+}
---
+++ src/main/java/com/gulcancelik/Veteriner.java
@@ -0,0 +1,8 @@
+package com.gulcancelik;
+
+public class Veteriner {
+
+    public Veteriner() {
+        System.out.println("Veteriner");
+    }
+}
